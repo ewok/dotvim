db10a91076bffdbad60bcf60a1b9d950cb2f8bea
Artur Taranchiev <ewok@ewok.ru>
2020-06-17 02:41:20 +0300

    Update installer

--- install_lnx.sh
+++ install_lnx.sh
@@ -1,2 +1,2 @@
-cat vimrc| grep '" inst:' | awk '{print "cd ~/.vim/pack/"$4";git pull --rebase"}' | bash
-cat vimrc| grep '" inst:' | awk '{print "git clone --recurse-submodules "$3" ~/.vim/pack/"$4}' | bash 
+cat vimrc| grep '" inst:' | awk '{print "cd ~/.vim/pack/"$4"/"$5"/"$6";git pull --rebase"}' | bash
+cat vimrc| grep '" inst:' | awk '{print "git clone --recurse-submodules "$3" ~/.vim/pack/"$4"/"$5"/"$6}' | bash 
