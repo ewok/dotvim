19a5226968a93ec6a845bfeb793f817a7598e345
Artur Taranchiev <ewok@ewok.ru>
2020-11-12 11:26:22 +0300

    To submodules

--- install_lnx.sh
+++ install_lnx.sh
@@ -1,4 +1,6 @@
 #!/bin/env bash
 
-cat vimrc| grep '" inst:' | awk '{print "test -d ~/.vim/pack/"$4"/"$5"/"$6" || git clone --depth 1  --recurse-submodules "$3" ~/.vim/pack/"$4"/"$5"/"$6}' | bash
-cat vimrc| grep '" inst:' | awk '{print "cd ~/.vim/pack/"$4"/"$5"/"$6";git pull --rebase"}' | bash
+#cat vimrc| grep '" inst:' | awk '{print "test -d ~/.vim/pack/"$4"/"$5"/"$6" || git clone --depth 1  --recurse-submodules "$3" ~/.vim/pack/"$4"/"$5"/"$6}' | bash
+#cat vimrc| grep '" inst:' | awk '{print "cd ~/.vim/pack/"$4"/"$5"/"$6";git pull --rebase"}' | bash
+
+git submodule update --init --recursive
